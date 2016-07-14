ae09f441a8b2a35787ece2ee657f966c20f8987c
AkhilaYeruva <akhila1704@gmail.com>
2016-07-14 16:31:05 -0700

    Intial checking

---
+++ Pizza/src/Pizza.java
@@ -0,0 +1,13 @@
+
+public class Pizza {
+private float totalPrice = 0;
+
+public float getTotalPrice() {
+	return totalPrice;
+}
+
+public void setTotalPrice(float totalPrice) {
+	this.totalPrice = totalPrice;
+}
+
+}
